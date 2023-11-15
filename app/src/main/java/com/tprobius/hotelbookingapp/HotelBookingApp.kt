e40b7c29be12f254b09770543b55f357cd76d2ef
Заремба Виктория <dviktoriyas@gmail.com>
2023-11-15 22:07:27 +0700

    Added HotelBookingApp class

---
+++ app/src/main/java/com/tprobius/hotelbookingapp/HotelBookingApp.kt
@@ -0,0 +1,20 @@
+package com.tprobius.hotelbookingapp
+
+import android.app.Application
+import com.tprobius.hotelbookingapp.di.navigationModule
+import com.tprobius.hotelbookingapp.di.netModule
+import com.tprobius.hotelbookingapp.di.viewModelModule
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class HotelBookingApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            modules(netModule, viewModelModule, navigationModule)
+        }
+    }
+}
